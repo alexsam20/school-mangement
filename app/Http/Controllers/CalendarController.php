63f992504062a5a3c485120989ed5c55c9b245f7
Alex SaM <alsam3637@gmail.com>
2024-10-15 20:16:21 -0400

    Student MyCalendar Exam Timetable.

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -3,16 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\Models\ClassSubject;
+use App\Models\ExamSchedule;
 use App\Models\Weeks;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CalendarController extends Controller
 {
+    // timetable
     public function myCalendar()
     {
+        $class_id = Auth::user()->class_id;
+        $data['getRecordsTimeTbl'] = $this->getTimetable($class_id);
+        $data['getRecordsExamTimeTbl'] = $this->getExamTimetable($class_id);
+        $data['header_title'] = 'My Calendar - ';
+
+        return view('student.my_calendar', $data);
+    }
+
+    public function getExamTimetable($classId)
+    {
+        $getExams = ExamSchedule::getExam($classId);
         $result = [];
-        $getRecords = ClassSubject::mySubject(Auth::user()->class_id);
+        foreach ($getExams as $exam) {
+
+            $dataExams = [];
+            $dataExams['name'] = $exam->exam_name;
+            $getExamTimetables = ExamSchedule::getExamTimetable($exam->exam_id, $classId);
+            $resultSubjects = [];
+
+            foreach ($getExamTimetables as $examTimetable) {
+                $dataExamTable = [];
+                $dataExamTable['subject_name'] = $examTimetable->subject_name;
+                $dataExamTable['exam_date'] = $examTimetable->exam_date;
+                $dataExamTable['start_time'] = $examTimetable->start_time;
+                $dataExamTable['end_time'] = $examTimetable->end_time;
+                $dataExamTable['room_number'] = $examTimetable->room_number;
+                $dataExamTable['full_marks'] = $examTimetable->full_marks;
+                $dataExamTable['passing_marks'] = $examTimetable->passing_marks;
+
+                $resultSubjects[] = $dataExamTable;
+            }
+            $dataExams['exam'] = $resultSubjects;
+            $result[] = $dataExams;
+        }
+
+        return $result;
+    }
+
+    public function getTimetable($class_id)
+    {
+        $result = [];
+        $getRecords = ClassSubject::mySubject($class_id);
         foreach ($getRecords as $value) {
             $dataS['name'] = $value->subject_name;
             $getWeek = Weeks::getRecords();
@@ -27,14 +69,9 @@ class CalendarController extends Controller
                 $week[] = array_merge($dataWeek, $times);
             }
             $dataS['week'] = $week;
-            if (empty($dataS['week']))
-                continue;
             $result[] = $dataS;
         }
 
-        $data['getRecordsTimeTbl'] = $result;
-        $data['header_title'] = 'My Calendar - ';
-
-        return view('student.my_calendar', $data);
+        return $result;
     }
 }
